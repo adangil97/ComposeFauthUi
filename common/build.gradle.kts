7ebf05dfc8f2553fbc4233a099466d17a1f9a48d
Adán Castillo <adan.gil97@gmail.com>
2025-09-23 14:24:01 -0600

    Increment project version and remove debug print statements
    The project version has been updated from 1.2.2 to 1.2.3.
    Several `println` statements used for debugging purposes in `FauthUiContent.android.kt` have been removed.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -101,7 +101,7 @@ mavenPublishing {
 
     signAllPublications()
 
-    coordinates("mx.empos", "composefauthui", "1.2.2")
+    coordinates("mx.empos", "composefauthui", "1.2.3")
 
     pom {
         name.set("Compose Fauth Ui")
